f28db2e1396aff48c7897a99688d3f483e753aed
seria_ati <61446626+seriaati@users.noreply.github.com>
2022-03-23 23:14:23 +0800

    moved to genshin.py

--- real-time-notes.py
+++ real-time-notes.py
@@ -1,13 +1,20 @@
-import genshinstats as gs
-gs.set_cookie(ltuid=7368957, ltoken="X5VJAbNxdKpMp96s7VGpyIBhSnEJr556d5fFMcT5")
+import asyncio
+import genshin
 
-uid = 901211014
-notes = gs.get_notes(uid)
-time = (notes['max_resin']-notes['resin'])*8/60
-hours = int(time)
-minutes = (time*60) % 60.
-print(f"User UID: {uid}")
-print(f"Current resin: {notes['resin']}/{notes['max_resin']}")
-print(f"Resin full in %d hours %02d minutes" % (hours, minutes))
-print(f"Current realm currency: {notes['realm_currency']}/{notes['max_realm_currency']}")
-print(f"Expeditions completed: {len(notes['expeditions'])}/{notes['max_expeditions']}")
+async def main():
+    client = genshin.GenshinClient()
+    cookies = genshin.get_browser_cookies()
+    client.set_cookies(cookies)
+
+    notes = await client.get_notes(901211014)
+    time = notes.until_resin_recovery
+    hours, minutes = divmod(time // 60, 60)
+    print(f"Current resin: {notes.current_resin}/{notes.max_resin}")
+    print(f"Resin full in {hours:.0f} hours {minutes:.0f} minutes")
+    print(f"Comissions left: {notes.completed_commissions}/{notes.max_comissions}")
+    print(f"Current realm currency: {notes.current_realm_currency}/{notes.max_realm_currency}")
+    print(f"Expeditions finished: {sum(expedition.finished for expedition in notes.expeditions)}")
+
+    await client.close()
+
+asyncio.run(main())
